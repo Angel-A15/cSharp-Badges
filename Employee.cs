3b61b932479f41b1109ed01a5fb365d86d4d0431
Angel-A15 <aasupertwix15@gmail.com>
2023-12-02 03:01:29 -0600

    finished set up for compiler to store all employee info successfully, working generating security badges

--- Employee.cs
+++ Employee.cs
@@ -3,10 +3,10 @@ namespace CatWorx.BadgeMaker
   class Employee
   {
     // properties
-    public string FirstName;
-    public string LastName;
-    public int Id;
-    public string PhotoUrl;
+    private string FirstName;
+    private string LastName;
+    private int Id;
+    private string PhotoUrl;
     public Employee(string firstName, string lastName, int id, string photoUrl)
     {
         FirstName = firstName;
@@ -18,40 +18,13 @@ namespace CatWorx.BadgeMaker
     public string GetFullName() {
         return FirstName + " " + LastName;
     }
+
+    public int GetId() {
+        return Id;
+    }
+
+    public string GetPhotoUrl() {
+        return PhotoUrl;
+    }
   }
 }
-
-// namespace CatWorx.BadgeMaker
-// {
-//   class Employee
-//   {
-//     private string FirstName;
-//     private string LastName;
-//     private int Id;
-//     private string PhotoUrl;
-//     public Employee(string firstName, string lastName, int id, string photoUrl){
-//         FirstName = firstName;
-//         LastName = lastName;
-//         Id = id;
-//         PhotoUrl = photoUrl;
-//     }
-
-//     public string GetFullName(){
-
-//         return FirstName + " " + LastName;
-//     }
-
-//     public int GetId() {
-//         return Id;
-//     }
-
-//     public string GetPhotoUrl(){
-//         return PhotoUrl;
-//     }
-
-//     public string GetCompanyName() {
-//       return "Cat Worx";
-//     }
-
-//   }
-// }
